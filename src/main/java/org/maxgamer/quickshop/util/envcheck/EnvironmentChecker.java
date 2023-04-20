ab04742e162178490e27c323ad7e341107a1057a
KASUNHapangama <88395585+KASUNHapangama@users.noreply.github.com>
2023-04-20 09:27:22 +0530

    Update EnvironmentChecker.java
    Removed Security Check

--- src/main/java/org/maxgamer/quickshop/util/envcheck/EnvironmentChecker.java
+++ src/main/java/org/maxgamer/quickshop/util/envcheck/EnvironmentChecker.java
@@ -28,14 +28,11 @@ import org.maxgamer.quickshop.api.shop.AbstractDisplayItem;
 import org.maxgamer.quickshop.api.shop.DisplayType;
 import org.maxgamer.quickshop.shop.VirtualDisplayItem;
 import org.maxgamer.quickshop.util.GameVersion;
-import org.maxgamer.quickshop.util.JsonUtil;
 import org.maxgamer.quickshop.util.MsgUtil;
 import org.maxgamer.quickshop.util.ReflectFactory;
 import org.maxgamer.quickshop.util.Util;
-import org.maxgamer.quickshop.util.security.JarVerifyTool;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.lang.reflect.Method;
 import java.net.URLDecoder;
 import java.util.ArrayList;
@@ -46,8 +43,6 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Properties;
-import java.util.jar.JarEntry;
-import java.util.jar.JarFile;
 import java.util.logging.Level;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
@@ -191,64 +186,6 @@ public final class EnvironmentChecker {
             return false;
         }
     }
-
-    @SneakyThrows
-    @EnvCheckEntry(name = "Signature Verify", priority = 0, stage = {EnvCheckEntry.Stage.ON_LOAD, EnvCheckEntry.Stage.ON_ENABLE})
-    public ResultContainer securityVerify() {
-        JarVerifyTool tool = new JarVerifyTool();
-        JarFile jarFile = null;
-        try {
-            ClassLoader loader = this.getClass().getClassLoader();
-            try (InputStream stream1 = loader.getResourceAsStream("META-INF/MANIFEST.MF");
-                 InputStream stream2 = loader.getResourceAsStream("META-INF/SELFSIGN.DSA");
-                 InputStream stream3 = loader.getResourceAsStream("META-INF/SELFSIGN.SF")) {
-                if (stream1 == null || stream2 == null || stream3 == null) {
-                    if(stream1 == null){
-                        this.reportMaker.signatureFileMissing("META-INF/MANIFEST.MF");
-                    }
-                    if(stream2 == null){
-                        this.reportMaker.signatureFileMissing("META-INF/SELFSIGN.DSA");
-                    }
-                    if(stream3 == null){
-                        this.reportMaker.signatureFileMissing("META-INF/SELFSIGN.SF");
-                    }
-                    plugin.getLogger().warning("The signature could not be found! The QuickShop jar has been modified or you're running a custom build.");
-                    return new ResultContainer(CheckResult.KILL_SERVER, "Security risk detected, QuickShop jar has been modified.");
-                }
-            }
-            String jarPath = this.getClass().getProtectionDomain().getCodeSource().getLocation().getFile();
-            jarPath = URLDecoder.decode(jarPath, "UTF-8");
-            Util.debugLog("JarPath selected: " + jarPath);
-            jarFile = new JarFile(jarPath);
-            List<JarEntry> modifiedEntry = tool.verify(jarFile);
-            if (modifiedEntry.isEmpty()) {
-                return new ResultContainer(CheckResult.PASSED, "The jar is valid. No issues detected.");
-            } else {
-                modifiedEntry.forEach(jarEntry -> {
-                    this.reportMaker.signatureVerifyFail(jarEntry);
-                    plugin.getLogger().warning(">> Modified Class Detected <<");
-                    plugin.getLogger().warning("Name: " + jarEntry.getName());
-                    plugin.getLogger().warning("CRC: " + jarEntry.getCrc());
-                    plugin.getLogger().warning(JsonUtil.getGson().toJson(jarEntry));
-                });
-                plugin.getLogger().severe("QuickShop detected that the jar has been modified! This is usually caused by the file being corrupted or virus infected.");
-                plugin.getLogger().severe("To prevent severe server failure, QuickShop has been disabled.");
-                plugin.getLogger().severe("For further information, Please join our support Discord server: https://discord.com/invite/bfefw2E.");
-                return new ResultContainer(CheckResult.KILL_SERVER, "Security risk detected, QuickShop jar has been modified.");
-            }
-        } catch (Exception ioException) {
-            plugin.getLogger().log(Level.WARNING, "ALERT: QuickShop cannot validate itself. This may be caused by you having deleted QuickShop's jar while the server is running.", ioException);
-            return new ResultContainer(CheckResult.KILL_SERVER, "Failed to validate digital signature! Security may be compromised!");
-        } finally {
-            if (jarFile != null) {
-                try {
-                    jarFile.close();
-                } catch (IOException ignored) {
-                }
-            }
-        }
-    }
-
 
     @EnvCheckEntry(name = "Plugin Manifest Check", priority = 1, stage = {EnvCheckEntry.Stage.ON_LOAD, EnvCheckEntry.Stage.ON_ENABLE})
     public ResultContainer manifestCheck() {
